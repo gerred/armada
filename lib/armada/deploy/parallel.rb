860c515bfa3de45f2cc4166e0249e66c4229c0ba
Darrell Hamilton <dhamilton@rallydev.com>
2014-10-24 12:24:29 -0600

    First stab at using assigned host ports

--- lib/armada/deploy/parallel.rb
+++ lib/armada/deploy/parallel.rb
@@ -23,12 +23,20 @@ module Armada
             container.start
 
             if @options[:health_check]
-              raise "Health check failed! - #{host}" unless Armada::Connection::HealthCheck.new(host, @options[:health_check_port],
-                                                                                                      @options[:health_check_endpoint],
-                                                                                                      @options[:health_check_delay],
-                                                                                                      @options[:health_check_retries],
-                                                                                                      @options[:ssh_gateway],
-                                                                                                      @options[:ssh_gateway_user]).run
+              port_bindings = container.port_bindings
+              health_check_binding = port_bindings["#{@options[:health_check_port]}/tcp"].first
+
+              health_check = Armada::Connection::HealthCheck.new(
+                host,
+                health_check_binding["HostPort"],
+                @options[:health_check_endpoint],
+                @options[:health_check_delay],
+                @options[:health_check_retries],
+                @options[:ssh_gateway],
+                @options[:ssh_gateway_user]
+              )
+
+              raise "Health check failed! - #{host}" unless health_check.run
             end
           end
         rescue Exception => e
